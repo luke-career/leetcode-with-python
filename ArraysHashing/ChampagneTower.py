df0c1b4d42d85c367118c9bfc4f5bba34b4bdffe
taolu-career <taolu.career@gmail.com>
2024-12-14 23:39:32 -0500

    leetcode

--- ArraysHashing/ChampagneTower.py
+++ ArraysHashing/ChampagneTower.py
@@ -2,8 +2,7 @@ class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
      
         arr = [[0.0] * (i + 1) for i in range(query_row + 1)]
-       # row,col = query_row + 1,query_glass + 1 
-       # arr = [[0 for _ in range(col)] for _ in range(row)]
+     
         arr[0][0] = poured
         for i in range(len(arr) - 1):
             for j in range(i + 1):
